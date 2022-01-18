58d4334f8aab5c4ef488f6ea91df361939d09915
pupsik <iliaman1993@gmail.com>
2022-01-18 22:07:33 +0300

    added string operations.py

---
+++ string and list/string operations.py
@@ -0,0 +1,39 @@
+# 1 task
+print('Я стану крутым программистом!\n'*3)
+
+# 2 task
+print('W'*777)
+
+# 3 task
+print('Лев Николаевич Толстой написал "Война и мир"')
+
+# 4 task
+print(input(), input(), sep = '\n')
+
+# 5 task
+a = input()
+b = input()
+c = input()
+print(c, b, a, sep = '\n')
+
+# 6 task
+a = input()
+print(a, a, a, a, sep = ' ')
+
+# 7 task
+a = input()
+print(len(a))
+
+# 8 task
+a, b = input(), input()
+print(b + a)
+
+# 9 task
+a = input()
+print(a*3)
+
+# 10 task
+a, b, c = map(str, input().split())
+print('Simvol code ' + a + ' is ' + str(ord(a)) + '.')
+print('Simvol code ' + b + ' is ' + str(ord(b)) + '.')
+print('Simvol code ' + c + ' is ' + str(ord(c)) + '.')
