9ddde05bde4552641783803c3008e20b9d6ac4d2
pupsik <iliaman1993@gmail.com>
2022-01-18 22:28:55 +0300

    added string slices and idexes.py

---
+++ string and list/string slices and idexes.py
@@ -0,0 +1,35 @@
+# 1 task
+s = input()
+print(s[0])
+
+# 2 task
+s = input()
+print(s[-1])
+
+# 3 task
+s = input()
+print(s[:4])
+
+# 4 task
+s = input()
+print(s[-4:])
+
+# 5 task
+s = input()
+print(s[::2])
+
+# 6 task
+s = input()
+print(s[1::2])
+
+# 7 task
+s = input()
+print(s[::-1])
+
+# 8 task
+s = input()
+print(s[-1::-3])
+
+# 9 task
+s = input()
+print(s[-1] + s[:len(s) - 1])
