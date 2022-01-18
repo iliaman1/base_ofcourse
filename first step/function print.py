dae859c0b37c94111d9ef8ceeaf7a22fc62cd066
pupsik <iliaman1993@gmail.com>
2022-01-18 14:39:05 +0300

    added next step "print"

---
+++ first step/function print.py
@@ -0,0 +1,18 @@
+# 1 task
+print('Привет, Мир!')
+
+# 2 task
+a, b, c = map(int, input().split())
+print(a, b, c, sep=',')
+
+# 3 task
+a = int(input())
+print(a - 1, a, a + 1, sep='<')
+
+# 4 task
+a, b, c = input(), input(), input()
+print(a, b, c, sep='---')
+
+# 5 task
+a = input()
+print(1, 2, 3, 4, 5, sep=a)
