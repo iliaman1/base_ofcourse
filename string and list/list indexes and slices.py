b0187f998bfeefebd7769f8c5eea1fd7722e6332
pupsik <iliaman1993@gmail.com>
2022-01-23 14:31:16 +0300

    added list indexes and slices and tasks for this.

---
+++ string and list/list indexes and slices.py
@@ -0,0 +1,26 @@
+# 1 task
+a = list(map(int, input().split()))
+print(a[1])
+
+# 2 task
+a = list(map(int, input().split()))
+print(a[2:5])
+
+# 3 task
+a = list(map(int, input().split()))
+print(a[-3:])
+
+# 4 task
+a = list(map(int, input().split()))
+print(a[1::3])
+
+# 5 task
+a = list(map(int, input().split()))
+print(a[::-1])
+
+# 6 task
+top = ['Игра престолов', 'Шерлок', 'Друзья', 'Во все тяжкие', 'Доктор Хаус', 'Теория большого взрыва', 'Бригада']
+top[2] = "Настоящий детектив"
+top[-1] = "Сверхъестественное"
+print(top)
+
